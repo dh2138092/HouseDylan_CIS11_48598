bab5e9e100511b11756e3de8f77f8d2c2e6e9c46
Dylan House <dhouse08@gmail.com>
2014-10-22 17:33:33 -0700

    Completed problem 1

--- Hmwk/Midterm/problem1.s
+++ Hmwk/Midterm/problem1.s
@@ -1,14 +1,13 @@
 .data
-message1 : .asciz "Please enter <# of hours worked> and <hourly pay>:\n"
+message1 : .asciz "\nPlease enter <hourly pay rate> and <# of hours worked during shift>:\n\n"
 format   : .asciz "%d %d"
-test_message : .asciz "You entered %d and %d\n"
-message2 : .asciz "The gross pay for %d hours worked at $%d per hour is $%d\n"
+message2 : .asciz "\nThe gross pay for the shift at $%d per hour for %d hour(s) is $%d.\n\n"
 
 .text
 calculate:
 	push {lr}
-			               /* r1 is numOfHours */
-	                               /* r2 is hourlyRate */
+			               /* r1 is hourlyRate */
+	                               /* r2 is numOfHours */
 	mov r3, #0                     /* r3 is grossPay */
 	mov r4, #2                     /* r4 is scaleFactor for double-time */
 	mov r5, #3                     /* r5 is scaleFactor for triple-time */
@@ -23,14 +22,18 @@ calculate:
 			bgt if_gt_20  /* else (numOfHours > 20) */
 				if_le_20:
 					mul r3, r1, r2   /* grossPay = numOfHours * hourlyRate */
+					pop {lr}
+					bx lr
 				if_gt_20:
 					mul r6, r4, r2  /* hourlyRate = hourlyRate * 2 */
 					mul r3, r1, r6  /* grossPay = numOfHours * hourlyRate */
+					pop {lr}
+					bx lr
 		if_gt_40:
 			mul r6, r5, r2                  /* hourlyRate = hourlyRate * 3 */
 			mul r3, r1, r6                  /* grossPa = numfHours * hourlyRate */
-	pop {lr}
-	bx lr
+			pop {lr}
+			bx lr
 
 	.global main
 
@@ -45,14 +48,7 @@ main:
 	mov r2, sp                      /* Set variable of the stack as b */
 	add r1, r2, #4                  /* and second value as a of scanf */
 	bl scanf
-@@@@@@@@@@@@@@@@@@@
-	add r1, sp, #4                  /* Place sp+4 -> r1 */
-	ldr r1, [r1]                    /* Load the integer a read by scanf into r1 */
-	ldr r2, [sp]                    /* Load the integer b read by scanf into r2 */
 
-	ldr r0, address_of_test_message
-	bl printf
-@@@@@@@@@@@@@@@@@@@
 	add r1, sp, #4
 	ldr r1, [r1]
 	ldr r2, [sp]
@@ -66,7 +62,6 @@ main:
 	bx lr                           /* Exit main */
 address_of_message1 : .word message1
 address_of_format   : .word format
-address_of_test_message : .word test_message
 address_of_message2 : .word message2
 
 	.global printf
