81c1f2936033061509c71e819682a2dba47737dc
Dylan <dhouse08@gmail.com>
2014-11-14 19:39:46 -0800

    :clap: C++ implementation of temperature conversion program

---
+++ Hmwk/Temperature Conversion/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+void divMod(int&, int&, int&);
+void scaleLeft(int&, int&, int&);
+void addSub(int&, int&, int&, int&);
+void scaleRight(int&, int&, int&);
+
+void convertFtoC(int &, int &);
+
+int main(int argv, char *argc[]) 
+{
+	int r0 = 0,
+		r1 = 0,    // Divisor - ((F - 32) * 5) / r2
+		r2 = 9,    // Dividend - where r2 = 9
+		r4 = 0;    // User input - fahrenheit
+
+	convertFtoC(r1, r4);
+	divMod(r2, r0, r1);
+
+	cout << "\n" << r4 << " F is " << r0 << " C.\n\n";
+
+	return 0;
+}
+
+void convertFtoC(int &r1, int &r4)
+{
+	do {
+		cout << "Enter a temperature 32 <= F <= 212: ";
+		cin >> r4;
+	} while (r4 < 32 || r4 > 212);
+
+	r1 = (r4 - 32) * 5;
+}
+
+void scaleLeft(int &r1, int &r3, int &r2)
+{
+	do {
+		r3 <<= 1;   // Scale factor
+		r2 <<= 1;   // Subtraction factor
+	} while (r1 >= r2);
+	r3 >>= 1;       // Scale factor back
+	r2 >>= 1;       // Scale subtraction factor back
+}
+
+void scaleRight(int &r1, int &r3, int &r2)
+{
+	do {
+		r3 >>= 1;   // Division counter
+		r2 >>= 1;   // Mod/Remainder subtraction
+	} while (r1 < r2);
+}
+
+void addSub(int &r3, int &r2, int &r0, int &r1)
+{
+	do {
+		r0 += r3;                // Count the subtracted scale factor
+		r1 -= r2;                // Subtract the scaled mod
+		scaleRight(r1, r3, r2);  // Keep scaling right until < remainder
+	} while (r3 >= 1);           // Loop until division is complete
+}
+
+void divMod(int &r2, int &r0, int &r1)
+{
+	//Determine the quotient and remainder
+	r0 = 0;
+	int r3 = 1;
+
+	if (r1 >= r2)
+	{
+		scaleLeft(r1, r3, r2);
+		addSub(r3, r2, r0, r1);
+	}
+}
--- Proj/Project-1/main.cpp
+++ Proj/Project-1/main.cpp
@@ -12,10 +12,20 @@ int main(int argc, char* argv[])
 		turnNumber = 0;
 	char playAgain = 'Y';
 
+	cout << "            M A S T E R M I N D ! ! !\n\n"
+		 << "Created by Dylan House, CIS-11-48598\n\n"
+		 << "Crack the numerical code to win!\n\n"
+		 << "===============================================\n"
+		 << "GAME RULES : \n"
+		 << "1. The code is 3 digits long, with each digit\n"
+		 << "being between the numbers 1 thru 7.\n"
+		 << "2. You have 14 guesses to crack the code.\n"
+		 << "===============================================\n\n";
 
 
 	do {
 
+		// INITIALIZE NEW GAME
 		srand((unsigned int)time(NULL));
 		code1 = (rand() + 3266) % 7;
 		code2 = (rand() + 487809620) % 7;
@@ -25,7 +35,11 @@ int main(int argc, char* argv[])
 		cout << code1 << " " << code2 << " " << code3 << "\n\n"; // TEST
 
 		do {
+			cout << "================================================\n"
+				<< "                  Turn " << turnNumber << " / 14\n"
+				<< "================================================\n";
 
+			// Reset guess counters per each turn
 			numCorrect = 0;
 			numWrong = 0;
 
@@ -41,18 +55,34 @@ int main(int argc, char* argv[])
 			if (guess3 == code3) numCorrect++;
 			else numWrong++;
 
-			cout << "\nCP: " << numCorrect << "\nWP: " << numWrong << "\n\n";
+			if (numCorrect == 3)
+				break;
 
+			cout << "\n# in Correct Position        # in Wrong Position\n"
+				 << "---------------------        -------------------\n"
+				 << "          " << numCorrect << "                           " << numWrong << "\n\n";
+
+			// Increment turn counter
 			turnNumber++;
 
-		} while (numCorrect != 3 && numWrong != 0 || turnNumber != 14);
+		} while (turnNumber != 15);
 
-		std::cout << "Play again? (Y or N)   ";
+		if (turnNumber == 15)
+			cout << "You lose! The correct combo was " << code1 << " " << code2 << " " << code3 << "\n\n";
+		else
+			cout << "*************************************************\n"
+			     << "*******************YOU WON!!*********************\n"
+			     << "*************************************************\n";
+
+		cout << "Play again? (Y or N)   ";
 		cin.ignore(1, '\n');
 		playAgain = getchar();
 		cin.ignore(1, '\n');
+		cout << "\n\n";
 
 	} while (playAgain == 'Y' || playAgain == 'y');
+
+	cout << "\n\nGoodbye!\n\n";
 
 	return 0;
 }
