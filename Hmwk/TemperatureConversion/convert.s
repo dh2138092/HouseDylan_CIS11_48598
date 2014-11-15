9425b3965fb694832dac302b6613fc113772016c
Dylan House <dhouse08@gmail.com>
2014-11-14 23:04:26 -0800

    Made convertFtoC() in convert.s more portable

--- Hmwk/TemperatureConversion/convert.s
+++ Hmwk/TemperatureConversion/convert.s
@@ -8,36 +8,19 @@ scan:    .asciz "%d"
 
 convertFtoC:
 	push {lr}
-	sub sp, sp, #4
 
-	do_while:
-	ldr r0, addr_msg
-	bl printf
-
-	ldr r0, addr_scan
-	mov r1, sp
-	bl scanf
-
-	ldr r0, [sp]
-	cmp r0, #32
-	blt do_while
-	cmp r0, #212
-	bgt do_while
-
-	mov r4, r0           @ store user input in r4 so that I can print the value at end of main.s
-
-	/* Now calculate ((F - 32) * 5) / 9, where F is the user's input 32 <= F <= 212 */
+	/* Calculate ((F - 32) * 5), where F is the user's input 32 <= F <= 212 */
 	sub r0, r0, #32      @ r0 = F - 32
 	mov r1, #5
 	mul r0, r0, r1       @ r0 = r0 * 5
 
-	/* And then pass r1 and r2 into divMod to get r1 / r2 = final result */
+	/* Then (r0 / 9) to get final value of Fahrenheit -> Celsius conversion */
+	/* So, pass r1 and r2 into divMod to get r1 / r2 = Celsius */
 	mov r1, r0
 	mov r2, #9
 
 	bl divMod
 
-	add sp, sp, #4
 	pop {pc}
 	mov pc, lr
 
--- Hmwk/TemperatureConversion/main.s
+++ Hmwk/TemperatureConversion/main.s
@@ -1,22 +1,60 @@
 .data
 
-message: .asciz "\n%d F is ~%d C.\n\n"
+message_in:  .asciz "\nEnter a temperature 32 <= F <= 212: "
+message_out: .asciz "\n%d F is ~%d C.\n\n"
+scan:        .asciz "%d"
 
 .text
 
-	.global main
+get_input:
+	push {lr}
+	sub sp, sp, #4
 
-main:
+	/* Get input, but only accept values 32 <= x <= 212 */
+        do_while:
+        ldr r0, addr_msg_in
+        bl printf
+
+        ldr r0, addr_scan
+        mov r1, sp
+        bl scanf
+
+        ldr r0, [sp]
+        cmp r0, #32
+        blt do_while
+        cmp r0, #212
+        bgt do_while
+
+        mov r4, r0           @ store user input in r4 so that I can print the value at end of main.s
+
+	add sp, sp, #4
+	pop {pc}
+	mov pc, lr
+
+print_result:
 	push {lr}
 
-	bl convertFtoC
-
-	mov r1, r4            @ r1 is the user's input in fahrenheit
-	mov r2, r0            @ r0 is the user's input converted to celsius
-	ldr r0, addr_msg
-	bl printf
+        mov r1, r4            @ r1 is the user's input in fahrenheit
+        mov r2, r0            @ r0 is the user's input converted to celsius
+        ldr r0, addr_msg_out
+        bl printf
 
 	pop {pc}
 	mov pc, lr
 
-addr_msg: .word message
+	.global main
+main:
+	push {lr}
+
+	bl get_input
+
+	bl convertFtoC
+
+	bl print_result
+
+	pop {pc}
+	mov pc, lr
+
+addr_msg_in:  .word message_in
+addr_msg_out: .word message_out
+addr_scan:    .word scan
