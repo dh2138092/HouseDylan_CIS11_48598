357323ac6fb6abf9afeb0183ddb21c3e2573b306
Dylan House <dhouse08@gmail.com>
2014-11-26 12:38:54 -0800

    initial push of drag float

---
+++ Hmwk/DragCalc/float.s
@@ -0,0 +1,73 @@
+.data
+
+half:             .float 0.5
+rho:              .float 0.00237
+velocity:         .word 200
+pi:               .float 3.141592
+radius:           .word 6
+conv:             .float 0.006944
+dragCoeffecient:  .float 0.4
+message: .asciz "\nFloat Dynamic Pressure = %f lbs\nCross-Sectional Area x 32 = %f ft^2\nFloat Drag x 32 = %f lbs\n\n"
+
+.text
+
+	.global main
+main:
+	push {lr}
+	sub sp, sp, #24
+
+	@@ Calculate dynamic pressure -> s0
+
+	ldr r3, =half
+	ldr r4, =rho
+	ldr r5, =velocity
+	vldr s13, [r3]
+        vldr s14, [r4]
+	vldr s15, [r5]
+
+	vmov s0, s13
+	vmul.f32 s0, s0, s14
+	vmul.f32 s0, s0, s15
+	vmul.f32 s0, s0, s15
+
+	@@ Calculate cross-sectional area -> s2
+
+	ldr r4, =pi
+	ldr r5, =radius
+	ldr r6, =conv
+	vldr s14, [r4]
+	vldr s15, [r5]
+	vldr s16, [r6]
+
+	vmov s2, s14
+	vmul.f32 s2, s2, s15
+	vmul.f32 s2, s2, s15
+	vmul.f32 s2, s2, s16
+
+	@@ Calculate drag -> s4
+
+	ldr r4, =dragCoeffecient
+	vldr s14, [r4]
+
+	vmul.f32 s4, s0, s2
+	vmul.f32 s4, s4, s14
+
+	@@ Convert single-precision to double-precision to printf results
+
+	vcvt.f64.f32 d0, s0
+	vcvt.f64.f32 d1, s2
+	vcvt.f64.f32 d2, s4
+
+	@@ Print out results
+
+	ldr r0, =message
+	vmov r2, r3, d0
+	vstr d1, [sp]
+	vstr d2, [sp, #+8]
+	bl printf
+
+	add sp, sp, #24
+	pop {pc}
+
+exit:
+	mov pc, lr
---
+++ Hmwk/DragCalc/test2.s
@@ -0,0 +1,33 @@
+	.global main
+	.func main
+main:
+	sub sp, sp, #24
+
+	ldr r1, =value1
+	vldr s14, [r1]
+	vcvt.f64.f32 d0, s14
+
+	ldr r1, =value2
+	vldr s15, [r1]
+	vcvt.f64.f32 d1, s15
+
+	ldr r1, =value3
+	vldr s16, [r1]
+	vcvt.f64.f32 d2, s16
+
+	ldr r0, =string
+	vmov r2, r3, d0
+	vstr d1, [sp]
+	vstr d2, [sp, #+8]
+	bl printf
+
+	add sp, sp, #24
+
+	mov r7, #1
+	swi 0
+
+	.data
+value1: .float 1.54321
+value2: .float 5.1
+value3: .float 3.2
+string: .asciz "The FP values are %f and %f and %f\n"
