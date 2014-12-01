2138aa435a2a1e57e15dd838977cf7d52d59c450
Dylan House <dhouse08@gmail.com>
2014-12-01 11:55:58 -0800

    Temp. convert by floats

--- Hmwk/TempConvert/Comparison/float.s
+++ Hmwk/TempConvert/Comparison/float.s
@@ -4,25 +4,39 @@
 main:
 	sub sp, sp, #16
 
-	ldr r0, =fahrenheit
-	ldr r1, =factor
-	ldr r2, =factor2
-	vldr s14, [r0]
-	vldr s15, [r1]
-	vldr s16, [r2]
+	mov r3, #0
+	ldr r4, =50000000
 
-	vsub.f32 s0, s15, s14
-	vmul.f32 s0, s0, s16
+	loop:
+		ldr r0, =fahrenheit
+		ldr r1, =factor
+		ldr r2, =factor2
+		vldr s14, [r0]
+		vldr s15, [r1]
+		vldr s16, [r2]
 
-	vcvt.f64.f32 d0, s14
-	vcvt.f64.f32 d1, s0
+		vsub.f32 s12, s14, s15
+		vmul.f32 s12, s12, s16
+
+		vcvt.f64.f32 d0, s14
+		vcvt.f64.f32 d1, s12
+
+		add r3, r3, #1
+		cmp r3, r4
+		blt loop
 
 	ldr r0, =msg
 	vmov r2, r3, d0
 	vstr d1, [sp]
 	bl printf
 
+	ldr r0, =msg2
+	ldr r1, =50000000
+	bl printf
+
 	add sp, sp, #16
+
+exit:
 	mov r7, #1
 	swi 0
 
@@ -30,4 +44,5 @@ main:
 fahrenheit: .float 212
 factor:     .float 32
 factor2:     .float 0.555555
-msg: .asciz "\nNumbers: %f %f\n"
+msg: .asciz "\nConverting by floats...\n\n%f F is %f C\n"
+msg2: .asciz "Looped %d times\n\n"
