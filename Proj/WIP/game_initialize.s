7b3be0bb25c4d39b79c122b91621665f47da4ef1
Dylan House <dhouse08@gmail.com>
2014-12-03 11:00:47 -0800

    Added init code array func

---
+++ Proj/WIP/game_initialize.s
@@ -0,0 +1,74 @@
+.align 4
+codes: .skip 16
+
+.align 4
+gusses: .skip 16
+
+.text
+.global game_initialize
+game_initialize:
+	push {lr}
+
+	mov r0, #0                   /* r0 for time(r0) */
+	mov r1, #0                   /* r1 = remainder */
+	mov r2, #7                   /* r2 for randomNum % r2 */
+
+/*
+	mov r4, #0 @ code 1
+	mov r5, #0 @ code 2
+	mov r6, #0 @ code 3
+*/
+	mov r9, #0 @ correct counter
+	mov r10, #0 @ incorrect counter
+	mov r11, #1 @ turn counter
+	mov r12, #'Y' @ play again?
+
+	mov r0, #0
+	bl time		@@ time(r0)
+	bl srand	@@ Seed random num generator
+	bl rand		@@ r0 <- Random num
+
+	@@ Fill codes array with 3 random digits each 0 < x < 8
+	init_codes:
+		ldr r6, =codes		@@ Array of 3 codes
+		mov r3, #2
+		mov r4, #0		@@ Memory address pointer
+
+		b init_compare
+		init_loop:
+			mov r3, r3, asr #1
+			mul r1, r1, r3
+			mov r2, #7		@@ Don't want codes > 7
+			bl divmod
+			str r1, [r6, r4, lsl #2]
+
+			add r4, r4, #1
+			init_compare:
+				cmp r4, #3
+				ble init_loop
+
+/*
+	generate_1:
+	mov r1, r0, ASR #1
+	mov r2, #7
+	bl divMod
+	mov r4, r1
+
+	generate_2:
+        mov r1, r0, ASR #2
+	mov r3, #3
+ 	mul r1, r1, r3
+        mov r2, #7
+	bl divMod
+	mov r5, r1
+
+	generate_3:
+        mov r1, r0, ASR #3
+        mov r3, #4
+        mul r1, r1, r3
+        mov r2, #7
+	bl divMod
+	mov r6, r1
+*/
+	pop {pc}
+	mov pc, lr
---
+++ Proj/WIP/project.s
@@ -0,0 +1,51 @@
+	.data
+message_welcome:   .asciz "\n\n            M A S T E R M I N D ! ! !\n\nCreated by Dylan House, CIS-11-48598\n\nCrack the numerical code to win!\n\n\n***********************************************\n\nGAME RULES:\n\n1. The code is 3 digits long, with each digit\n   being between the numbers 1 thru 7.\n\n2. You have 14 guesses to crack the code.\n\n3. Enter one number at a time.\n\n***********************************************\n\n"
+message_win:       .asciz "*************************************************\n*******************YOU WON!!*********************\n*************************************************\n\n"
+message_lose:      .asciz "You lose! The correct code was %d %d %d\n\n"
+
+	.text
+	.global main
+main:
+	push {lr}
+
+	ldr r0, addr_of_message_welcome
+	bl printf
+
+	doWhile_r12_eq_Y:
+		bl game_initialize
+
+		doWhile_r11_lt_15:
+			bl game_main
+		cmp r11, #15
+		blt doWhile_r11_lt_15
+
+		cmp r9, #3    /* Use the # correct count to determine if round won or lost */
+		bge win
+		blt lose
+		win:
+			ldr r0, addr_of_message_win
+			b game_over
+		lose:
+			ldr r0, addr_of_message_lose
+			/* Prepare to printf secret code */
+		        mov r1, r4
+                	mov r2, r5
+                	mov r3, r6
+			b game_over
+
+	game_over:
+		bl printf
+		bl play_again
+
+		cmp r1, #'Y'
+		beq doWhile_r12_eq_Y
+		cmp r1, #'y'
+		beq doWhile_r12_eq_Y
+
+end:
+	pop {pc}
+	mov pc, lr
+
+addr_of_message_welcome:   .word message_welcome
+addr_of_message_win:       .word message_win
+addr_of_message_lose:      .word message_lose
